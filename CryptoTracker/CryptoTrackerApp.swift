b3981429c3a90cf744073bda44abe45b69485cc6
Jchillah <jchillah@gmail.com>
2025-03-12 11:29:43 +0100

    initialize commit: created the Project

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Michael Winkler on 12.03.25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
